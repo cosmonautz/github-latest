5d77a55c6e4afb2a95e1950e112544c5bf27ef36
Kevin Waldron <kevinmwaldron@gmail.com>
2019-03-15 18:34:26 -0700

    my changes

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ if __name__ == "__main__":
     #
     # 1. Retrieve a list of "events" associated with the given user name
     # 2. Print out the time stamp associated with the first event in that list.
+    resp = requests.get(f"https://api.github.com/users/{username}/events")
+    events = json.loads(resp.content)
 
-    print("COMPLETE THE TODOs")
+    print(events[0]['created_at'])
     
 
 
